9f22e3af5847f829ef9fbef9188b862ce2e9ce93
Itamar Pereira <itamartpereira@hotmail.com>
2020-05-27 16:26:34 -0300

    Finished seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the database"
+
+Flat.destroy_all
+
+puts "DB is clean"
+
+puts "Creating 5 flats"
+
+5.times do
+  Flat.create!(
+    name: Faker::Address.street_address,
+    address: Faker::Games::Zelda.location,
+    description: Faker::Movies::Ghostbusters.quote,
+    price_per_night: [50, 55, 60, 75, 90, 100, 105, 120, 132, 150].sample,
+    number_of_guests: rand(1..5)
+  )
+end
+
+puts "Done!"
+
